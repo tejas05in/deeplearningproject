8282411d90c984a5e33012609c58687d410d921c
Dr. TEJAS. J <tejas05in@yahoo.co.in>
2024-02-08 15:40:33 +0530

    data ingestion updated

--- Xray/components/data_ingestion.py
+++ Xray/components/data_ingestion.py
@@ -1,6 +1,4 @@
 import sys
-
-
 from Xray.cloud_storage.s3_operation import S3Operation
 # from Xray.constant.training_pipeline import *
 from Xray.entity.artifact_entity import DataIngestionArtifact
