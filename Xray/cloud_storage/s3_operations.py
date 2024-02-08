2dbde74f07b33f6ec860dc8bdd76c2e9213bc0a4
Dr. TEJAS. J <tejas05in@yahoo.co.in>
2024-02-08 15:27:06 +0530

    s3 integration

---
+++ Xray/cloud_storage/s3_operations.py
@@ -0,0 +1,25 @@
+import os
+import sys
+from Xray.exception import XRayException
+
+class S3Operation:
+    
+    def sync_folder_to_s3()->None:
+        try:
+            command: str = (
+                f" aws s3 sync {folder} s3://{bucket_name}/{bucket_folder_name}"
+            )
+
+            os.system(command)
+        except Exception as e:
+            raise XRayException(e,sys)
+    
+    def sync_folder_from_s3()->None:
+        try:
+            command: str = (
+                f"aws s3 sync s3://{bucket_name}/{bucket_folder_name}/{folder}"
+
+            )
+            os.system(command)
+        except Exception as e:
+            raise XRayException(e,sys)
