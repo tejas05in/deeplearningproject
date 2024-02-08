6da0a62aa130a1169c1e8f2b18a2ba29015232fa
Dr. TEJAS. J <tejas05in@yahoo.co.in>
2024-02-08 23:52:41 +0530

    updated the s3 bucket details

--- Xray/constant/training_pipeline/__init__.py
+++ Xray/constant/training_pipeline/__init__.py
@@ -7,7 +7,7 @@ TIMESTAMP: datetime = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
 
 ARTIFACT_DIR:str = "artifacts"
 
-BUCKET_NAME:str = "xraylungimgs"
+BUCKET_NAME: str = "xraylungimgstj"
 
 S3_DATA_FOLDER:str = "data"
 
