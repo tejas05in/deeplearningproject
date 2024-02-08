930f3a5ff0c7ce61dae9da644766624afcb481b8
Dr. TEJAS. J <tejas05in@yahoo.co.in>
2024-02-08 23:27:00 +0530

    data ingestion updated

--- Xray/cloud_storage/s3_operation.py
+++ Xray/cloud_storage/s3_operation.py
@@ -4,7 +4,7 @@ from Xray.exception import XRayException
 
 class S3Operation:
     
-    def sync_folder_to_s3()->None:
+    def sync_folder_to_s3(self, folder:str , bucket_name:str , bucket_folder_name: str)->None:
         try:
             command: str = (
                 f" aws s3 sync {folder} s3://{bucket_name}/{bucket_folder_name}"
@@ -14,7 +14,7 @@ class S3Operation:
         except Exception as e:
             raise XRayException(e,sys)
     
-    def sync_folder_from_s3()->None:
+    def sync_folder_from_s3(self, folder: str, bucket_name: str, bucket_folder_name: str) -> None:
         try:
             command: str = (
                 f"aws s3 sync s3://{bucket_name}/{bucket_folder_name}/{folder}"
